dfefe577b0f0f52a397b08686c046f5cc30eb04e
Chang Sophia <you@example.com>
2019-01-26 16:18:08 +0800

    Initial Commit

---
+++ changeGemGesture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  changeGemGesture
+//
+//  Created by Chang Sophia on 1/26/19.
+//  Copyright © 2019 Chang Sophia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
